f294662cd7289207cf3bf68889bb6a514ceb60cb
Uri Shamay <cmpxchg16@gmail.com>
2024-03-28 18:38:22 +0200

    Add support to the new OpenAI SDK 1.*

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 ## Overview
 
 Client SDK to send LLM interactions to the [Vibranium Dome System](https://github.com/genia-dev/vibraniumdome).
+Supports both the deprecated version of OpenAI Python SDK 0.28.1, and the new version 1.*
 
 ## Getting Started 
 
@@ -25,33 +26,21 @@ export VIBRANIUM_DOME_API_KEY=vibranium...
 Code sample:
 ```python
 import os
-import openai
+from openai import OpenAI
 
 from vibraniumdome_sdk import VibraniumDome
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+VibraniumDome.init(app_name="vibraniumdome_sdk_openai_test_app")
 
-VibraniumDome.init(app_name="openai_test_app")
-
-def main():
-    response = openai.ChatCompletion.create(
+client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
+completion = client.chat.completions.create(
     model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Who won the world series in 2020?"},
-        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-        {"role": "user", "content": "Where was it played?"}
-    ],
-    temperature=0,
-    request_timeout=5,
+    messages=[{"role": "user", "content": "Tell me a joke about opentelemetry"}],
     user="user-123456",
-    headers={"x-session-id": "abcd-1234-cdef"},)
+    extra_headers={"x-session-id": "abcd-1234-cdef"},
+)
 
-    print(response)
-
-
-if __name__ == "__main__":
-    main()
+print(completion.choices[0].message.content)
 ```
 
 
--- tests/test_vibranium_dome_sdk.py
+++ tests/test_vibranium_dome_sdk.py
@@ -1,30 +1,104 @@
 import os
 import unittest
-import openai
+import json
+from openai import OpenAI
 
 from vibraniumdome_sdk import VibraniumDome
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
-
-VibraniumDome.init(app_name="openai_test_app")
+VibraniumDome.init(app_name="vibraniumdome_sdk_openai_test_app")
 
 class TestVibraniumDomeSDK(unittest.TestCase):
     @unittest.skipUnless(os.environ.get("PYTHON_E2E_TESTS") is not None, "Not in e2e test environment")
-    def test_sanity(self):
-        response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": "Who won the world series in 2020?"},
-            {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-            {"role": "user", "content": "Where was it played?"}
-        ],
-        temperature=0,
-        request_timeout=5,
-        user="user-123456",
-        headers={"x-session-id": "abcd-1234-cdef"},)
+    def test_sanity_1(self):
+        client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
+        completion = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role": "user", "content": "Tell me a joke about opentelemetry"}],
+            user="user-123456",
+            extra_headers={"x-session-id": "abcd-1234-cdef"},
+        )
 
-        print(response)
+        print(completion.choices[0].message.content)
+            
+    @unittest.skipUnless(os.environ.get("PYTHON_E2E_TESTS") is not None, "Not in e2e test environment")
+    def test_sanity_2(self):
+
+        # Example dummy function hard coded to return the same weather
+        # In production, this could be your backend API or an external API
+        def get_current_weather(location, unit="fahrenheit"):
+            """Get the current weather in a given location"""
+            if "tokyo" in location.lower():
+                return json.dumps({"location": "Tokyo", "temperature": "10", "unit": unit})
+            elif "san francisco" in location.lower():
+                return json.dumps({"location": "San Francisco", "temperature": "72", "unit": unit})
+            elif "paris" in location.lower():
+                return json.dumps({"location": "Paris", "temperature": "22", "unit": unit})
+            else:
+                return json.dumps({"location": location, "temperature": "unknown"})
+        
+        client = OpenAI()
+
+        # Step 1: send the conversation and available functions to the model
+        messages = [{"role": "user", "content": "What's the weather like in San Francisco, Tokyo, and Paris?"}]
+        tools = [
+            {
+                "type": "function",
+                "function": {
+                    "name": "get_current_weather",
+                    "description": "Get the current weather in a given location",
+                    "parameters": {
+                        "type": "object",
+                        "properties": {
+                            "location": {
+                                "type": "string",
+                                "description": "The city and state, e.g. San Francisco, CA",
+                            },
+                            "unit": {"type": "string", "enum": ["celsius", "fahrenheit"]},
+                        },
+                        "required": ["location"],
+                    },
+                },
+            }
+        ]
+        response = client.chat.completions.create(
+            user="user-123456",
+            model="gpt-3.5-turbo-0125",
+            messages=messages,
+            tools=tools,
+            tool_choice="auto",  # auto is default, but we'll be explicit
+        )
+        response_message = response.choices[0].message
+        tool_calls = response_message.tool_calls
+        # Step 2: check if the model wanted to call a function
+        if tool_calls:
+            # Step 3: call the function
+            # Note: the JSON response may not always be valid; be sure to handle errors
+            available_functions = {
+                "get_current_weather": get_current_weather,
+            }  # only one function in this example, but you can have multiple
+            messages.append(response_message)  # extend conversation with assistant's reply
+            # Step 4: send the info for each function call and function response to the model
+            for tool_call in tool_calls:
+                function_name = tool_call.function.name
+                function_to_call = available_functions[function_name]
+                function_args = json.loads(tool_call.function.arguments)
+                function_response = function_to_call(
+                    location=function_args.get("location"),
+                    unit=function_args.get("unit"),
+                )
+                messages.append(
+                    {
+                        "tool_call_id": tool_call.id,
+                        "role": "tool",
+                        "name": function_name,
+                        "content": function_response,
+                    }
+                )  # extend conversation with function response
+            second_response = client.chat.completions.create(
+                model="gpt-3.5-turbo-0125",
+                messages=messages,
+            )  # get a new response from the model where it can see the function response
+            print(second_response)
 
 if __name__ == "__main__":
     unittest.main()
