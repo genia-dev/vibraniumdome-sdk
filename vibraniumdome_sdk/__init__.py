ff41d4fabeb40f282b61a646c5fdd54740188845
Uri Shamay <cmpxchg16@gmail.com>
2024-03-31 12:40:26 +0300

    bump dependency version + add support to disable_batch in OpenTelemetry

--- vibraniumdome_sdk/__init__.py
+++ vibraniumdome_sdk/__init__.py
@@ -1,18 +1,29 @@
 import os
+import sys
+import io
 
 from traceloop.sdk import Traceloop
 
 class VibraniumDome:
     @staticmethod
     def init(
-        app_name: str
+        app_name: str,
+        disable_batch=False
     ) -> None:
-        if 'VIBRANIUM_DOME_BASE_URL' not in os.environ:
-            os.environ['TRACELOOP_BASE_URL'] = "http://localhost:5001"
-        else:
-            os.environ['TRACELOOP_BASE_URL'] = os.environ['VIBRANIUM_DOME_BASE_URL']
+        try:
+            original_stdout = sys.stdout
+            if 'VIBRANIUM_DOME_BASE_URL' not in os.environ:
+                os.environ['TRACELOOP_BASE_URL'] = "http://localhost:5001"
+            else:
+                os.environ['TRACELOOP_BASE_URL'] = os.environ['VIBRANIUM_DOME_BASE_URL']
 
-        if 'VIBRANIUM_DOME_API_KEY' not in os.environ:
-            raise ValueError("VIBRANIUM_DOME_API_KEY is not set")
+            if 'VIBRANIUM_DOME_API_KEY' not in os.environ:
+                raise ValueError("VIBRANIUM_DOME_API_KEY is not set")
 
-        Traceloop.init(app_name=app_name, headers = {"Authorization": f"Bearer {os.environ['VIBRANIUM_DOME_API_KEY']}",})
+            sys.stdout = io.StringIO()
+
+            Traceloop.init(app_name=app_name,
+                           headers = {"Authorization": f"Bearer {os.environ['VIBRANIUM_DOME_API_KEY']}",},
+                           disable_batch=disable_batch)
+        finally:
+            sys.stdout = original_stdout
